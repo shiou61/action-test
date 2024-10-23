0581185fd2cab2ece5faf8557a5a33a4be11a2c4
shiou61 <shiou278@gmail.com>
2024-10-23 10:58:13 +0900

    add join

--- src/hello.py
+++ src/hello.py
@@ -6,4 +6,4 @@ rng = np.random.default_rng(0)
 filename = f"./logs/{datetime.now()}.txt"
 
 with open(filename, "w") as f:
-    f.write(rng.integers(1, 100, 100))
+    f.write(" ".join(rng.integers(1, 100, 100)))
