33fd5199ffd9709e97a4e5d4ac14e87c1c2b3785
shiou61 <shiou278@gmail.com>
2024-10-23 10:52:44 +0900

    add txt

--- src/hello.py
+++ src/hello.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 rng = np.random.default_rng(0)
 
-filename = f"./logs/{datetime.now()}"
+filename = f"./logs/{datetime.now()}.txt"
 
 with open(filename, "wb") as f:
     f.write(rng.integers(1, 100, 100))
